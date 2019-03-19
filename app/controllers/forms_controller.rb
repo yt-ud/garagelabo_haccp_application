c789a3fbff13241fa92bf85fd0bdf9979500072a
yutaueda <yufrom2018@gmail.com>
2019-03-20 04:48:31 +0900

    first product

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -8,6 +8,26 @@ class FormsController < ApplicationController
     api = Kintone::Api.new("garagelabo.cybozu.com", "WaPombVC4MyXbfJo1OQ1WUlPYcEvSbobIZviXONp")
     app = 39
 
+    checkboxes = {
+      "select1" => "帳票管理が面倒くさい\n",
+      "select2" => "業務が止まってしまう\n",
+      "select3" => "何から始めていいかわからない\n",
+      "select4" => "導入コストが高い\n",
+      "select5" => "専門知識を持っていない\n",
+      "select6" => "業界団体の手引書がまだ発行されていないので、対応方法がわからない\n",
+      "select7" => "自社内でだれが担当すればいいかがわからない\n",
+      "select8" => "社内に導入に反発する人が多い\n",
+      "select9" => "ISOやFSSC、JFSなどの規格認証を取得するかどうか悩んでいる\n",
+      "select10" => "自社内でだれが担当すればいいかがわからない\n"
+    }
+
+    haccp2 = ""
+    checkboxes.each { |key, value|
+      if params[:"#{key}"]
+        haccp2 += value
+      end
+    }
+
     record = {
       "date" => {"value" => params[:date]},
       "namekannji" => {"value" => params[:namekannji]},
@@ -16,8 +36,9 @@ class FormsController < ApplicationController
       "addresscompany" => {"value" => params[:addresscompany]},
       "phonenumber" => {"value" => params[:phonenumber]},
       "faxnumber" => {"value" => params[:faxnumber]},
-      "haccp1" => {"value" => params[:haccp]},
-      "haccp2" => {"value" => params[:select1]}
+      "haccp1" => {"value" => params[:haccp1]},
+      "haccp2" => {"value" => haccp2},
+      "other" =>  {"value" => params[:other]}
     }
 
     api.record.register(app, record)
