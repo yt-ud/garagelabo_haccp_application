f4b2248e23732b5fb5b63de13d38f7f34e28517b
yutaueda <yufrom2018@gmail.com>
2019-03-20 03:22:17 +0900

    almost complete

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,10 +1,26 @@
+require 'kintone'
 class FormsController < ApplicationController
 
   def entry
   end
 
   def post
-        
+    api = Kintone::Api.new("garagelabo.cybozu.com", "WaPombVC4MyXbfJo1OQ1WUlPYcEvSbobIZviXONp")
+    app = 39
+
+    record = {
+      "date" => {"value" => params[:date]},
+      "namekannji" => {"value" => params[:namekannji]},
+      "namefurigana" => {"value" => params[:namefurigana]},
+      "namecompany" => {"value" => params[:namecompany]},
+      "addresscompany" => {"value" => params[:addresscompany]},
+      "phonenumber" => {"value" => params[:phonenumber]},
+      "faxnumber" => {"value" => params[:faxnumber]},
+      "haccp1" => {"value" => params[:haccp]},
+      "haccp2" => {"value" => params[:select1]}
+    }
+
+    api.record.register(app, record)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'forms#entry'
   get '/entry', to: 'forms#entry'
   post '/posts', to: 'forms#post'
 end
