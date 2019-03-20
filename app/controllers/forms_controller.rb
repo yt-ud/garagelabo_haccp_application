66f388730c23c2f29fe41af7254162d9074d2a3c
yutaueda <yufrom2018@gmail.com>
2019-03-20 22:19:55 +0900

    add C column

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -9,16 +9,16 @@ class FormsController < ApplicationController
     app = 39
 
     checkboxes = {
-      "select1" => "帳票管理が面倒くさい\n",
-      "select2" => "業務が止まってしまう\n",
-      "select3" => "何から始めていいかわからない\n",
-      "select4" => "導入コストが高い\n",
-      "select5" => "専門知識を持っていない\n",
-      "select6" => "業界団体の手引書がまだ発行されていないので、対応方法がわからない\n",
-      "select7" => "自社内でだれが担当すればいいかがわからない\n",
-      "select8" => "社内に導入に反発する人が多い\n",
-      "select9" => "ISOやFSSC、JFSなどの規格認証を取得するかどうか悩んでいる\n",
-      "select10" => "自社内でだれが担当すればいいかがわからない\n"
+      "select1" => "・帳票管理が面倒くさい。\n",
+      "select2" => "・業務が止まってしまう。\n",
+      "select3" => "・何から始めていいかわからない。\n",
+      "select4" => "・導入コストが高い。\n",
+      "select5" => "・専門知識を持っていない。\n",
+      "select6" => "・業界団体の手引書がまだ発行されていないので、対応方法がわからない。\n",
+      "select7" => "・自社内でだれが担当すればいいかがわからない。\n",
+      "select8" => "・社内に導入に反発する人が多い。\n",
+      "select9" => "・ISOやFSSC、JFSなどの規格認証を取得するかどうか悩んでいる。\n",
+      "select10" => "・自社内でだれが担当すればいいかがわからない。\n"
     }
 
     haccp2 = ""
@@ -38,9 +38,9 @@ class FormsController < ApplicationController
       "faxnumber" => {"value" => params[:faxnumber]},
       "haccp1" => {"value" => params[:haccp1]},
       "haccp2" => {"value" => haccp2},
-      "other" =>  {"value" => params[:other]}
+      "other" =>  {"value" => params[:other]},
+      "haccp3" => {"value" => params[:haccp3]}
     }
-
     api.record.register(app, record)
   end
 
